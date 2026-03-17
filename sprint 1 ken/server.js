e3303aa61568d25bcb1d8e1b1ea7fa68f836052a
keniaKen <salgado.k02@mymail.sxu.edu>
2026-03-17 02:38:26 -0500

    these changes are the running page where you can update the likes it will go to the database

--- sprint 1 ken/server.js
+++ sprint 1 ken/server.js
@@ -4,6 +4,7 @@ const app = express();
 
 app.use(express.json());
 
+app.use(express.urlencoded({ extended: true }));
 const db = mysql.createConnection({
     host: "localhost",
     user: "root",
@@ -49,4 +50,34 @@ app.get("/liked-items", (req, res) => {
         if (err) return res.json({ success: false });
         res.json({ success: true, items: results });
     });
+});
+
+app.post("/add-item", (req, res) => {
+    const { designerName, imagePath } = req.body;
+
+    const sql = `INSERT INTO Items (DesignerName, ImagePath) VALUES (?, ?)`;
+
+    db.query(sql, [designerName, imagePath], (err, result) => {
+        if (err) {
+            console.log(err);
+            return res.send("Error adding item.");
+        }
+        res.send("Item added successfully!");
+    });
+});
+app.get("/designerdash", (req, res) => res.sendFile(__dirname + "/designerdash.html"));
+
+app.get("/", (req, res) => res.sendFile(__dirname + "/index.html"));
+app.get("/about", (req, res) => res.sendFile(__dirname + "/aboutus.html"));
+app.get("/contact", (req, res) => res.sendFile(__dirname + "/contact.html"));
+app.get("/login", (req, res) => res.sendFile(__dirname + "/login.html"));
+app.get("/register", (req, res) => res.sendFile(__dirname + "/register.html"));
+app.get("/userdash", (req, res) => res.sendFile(__dirname + "/userdash.html"));
+app.get("/designerdash", (req, res) => res.sendFile(__dirname + "/designerdash.html"));
+
+
+
+
+app.listen(3000, () => {
+    console.log("Server running on http://localhost:3000");
 });
